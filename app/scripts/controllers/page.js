8d619ce81a753d2ff0358e36c99835dbdec106b0
Max Mulatz <klappradla@gmail.com>
2015-05-17 19:30:19 +0200

    tryout some api stuff

--- app/scripts/controllers/page.js
+++ app/scripts/controllers/page.js
@@ -4,7 +4,6 @@
 
 angular.module('tanzmobil')
   .controller('PageCtrl', function ($scope, $http, $location) {
-    console.log("location: " + $location.url());
 
     // get wp page by slug
     $http.get('http://localhost:8888/api/get_page/?slug=' + $location.url()).success(function(data) {
--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -5,9 +5,10 @@
 angular.module('tanzmobil')
   .controller('PostCtrl', function ($scope, $http, $routeParams) {
 
-    // get wp page by slug
+    // get wp post by slug
     $http.get('http://localhost:8888/api/get_post/?slug=' + $routeParams.post).success(function(data) {
       $scope.post = data.post;
+      console.log(data);
     });
     
   });
