0048118cb81f3c5be024836ce6577fb5c21de8e2
Max Mulatz <klappradla@gmail.com>
2015-06-06 14:43:20 +0200

    add wp service to query api

---
+++ app/scripts/services/wpservice.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tanzmobilApp.WpService
+ * @description
+ * # WpService
+ * Service in the tanzmobilApp.
+ */
+angular.module('tanzmobilApp')
+  .service('WpService', function ($http) {
+  // AngularJS will instantiate a singleton by calling "new" on this function
+
+    var apiUrl = 'http://localhost:8888/api/';
+
+    function allPosts() {
+      return queryApi('get_posts').then(function(response) {
+        return response.data.posts;
+      });
+    }
+
+    function recentPosts() {
+      return queryApi('get_recent_posts')
+        .then(function(response) {
+          return response.data.posts;
+        });
+    }
+
+    // get wp page by slug
+    function page(slug) {
+      return queryApi('get_page/?slug=' + slug)
+        .then(function(response) {
+          return response.data.page;
+        });
+    }
+
+    function post(slug) {
+      return queryApi('get_post/?slug=' + slug)
+        .then(function(response) {
+          return response.data.post;
+        });
+    }
+
+    function queryApi(url) {
+      return $http
+        .get(apiUrl + url)
+        .then(function(response) {
+          return response;
+        });
+    }
+
+    return {
+      allPosts: allPosts,
+      recentPosts: recentPosts,
+      page: page,
+      post: post
+    };
+
+  });
