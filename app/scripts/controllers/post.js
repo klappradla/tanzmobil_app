dee62ea42c4bc9489fc1e96d22144bba02fe1372
Max Mulatz <klappradla@gmail.com>
2015-06-18 19:54:08 +0200

    some work on post page

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -8,8 +8,9 @@
  * Controller of the tanzmobilApp
  */
 angular.module('tanzmobilApp')
-  .controller('PostCtrl', function ($scope, $routeParams, WpService) {
+  .controller('PostCtrl', function ($scope, $routeParams, $sce, WpService) {
     WpService.post($routeParams.post).then(function(response) {
       $scope.post = response;
+      $scope.postContent = $sce.trustAsHtml($scope.post.content);
     });
   });
