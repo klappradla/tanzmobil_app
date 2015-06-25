0602e2c7f1477163b07857ea3c4226ae6d28353c
Max Mulatz <klappradla@gmail.com>
2015-06-25 21:55:13 +0200

    color filters

--- app/scripts/directives/backgroundcolor.js
+++ app/scripts/directives/backgroundcolor.js
@@ -8,9 +8,11 @@
  */
 angular.module('tanzmobilApp')
   .directive('backgroundColor', function (categoryColor) {
+    var alpha = true;
+    
     return function (scope, element, attrs) {
       element.css({
-        'background-color': categoryColor.getColor(attrs.backgroundColor)
+        'background-color': categoryColor.getColor(attrs.backgroundColor, alpha)
         });
     };
   });
--- app/scripts/services/categorycolor.js
+++ app/scripts/services/categorycolor.js
@@ -9,16 +9,42 @@
  */
 angular.module('tanzmobilApp')
   .service('categoryColor', function () {
+    function getColorAlpha(category, alpha) {
+      alpha = alpha || false;
+      switch(category) {
+        case 'german':
+          if (alpha) {
+            return 'rgba(237,85,101,0.5)';  
+          }
+          return '#ED5565';
+        case 'english':
+          if (alpha) {
+            return 'rgba(252,110,81,0.5)';
+          }
+          return '#FC6E51';
+        case 'french':
+          if (alpha) {
+            return 'rgba(172,146,236,0.5)';
+          }
+          return '#AC92EC';
+        default:
+          if (alpha) {
+            return 'rgba(107,107,107,0.5)';
+          }
+          return '#6B6B6B';
+      }
+    }
+
     function getColor(category) {
       switch(category) {
         case 'german':
-          return 'rgba(0,153,6,0.5)';
+          return 'rgba(237,85,101,0.5)';
         case 'english':
-          return 'rgba(255,0,0,0.5)';
+          return 'rgba(252,110,81,0.5)';
         case 'french':
-          return 'rgba(13,73,255,0.5)';
+          return 'rgba(172,146,236,0.5)';
         default:
-          return 'rgba(51,51,51,0.5)';
+          return 'rgba(107,107,107,0.5)';
       }
     }
 
